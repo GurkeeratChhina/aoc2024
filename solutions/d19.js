7aded6fd5762300037cc59a331c443a9640c5171
GurkeeratChhina <gurkeerat.chhina@gmail.com>
2024-12-18 21:27:32 -0800

    d19 p1

---
+++ solutions/d19.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+
+const cache = new Map()
+
+const parse = function(filename) {
+    var text = fs.readFileSync(filename,'utf8').split("\n\n")
+    let towels = text[0].trim().match(/[a-z]+/g)
+    let arrangements = []
+    for (const line of text[1].trim().split("\n")) {
+        arrangements.push(line.trim())
+    }
+    return [towels, arrangements]
+}
+
+const is_possible = function(arrangement, towels) {
+    // console.log("testing", arrangement)
+    if (cache.has(arrangement)) {
+        // console.log("already calculated", arrangement)
+        return cache.get(arrangement)
+    }
+    if (arrangement.length == 0) {
+        cache.set(arrangement, true)
+        return true
+    }
+    for (let towel of towels) {
+        let regex = new RegExp('^'.concat(towel))
+        // console.log(regex)
+        // console.log(regex.test(arrangement))
+        if (regex.test(arrangement)) {
+            let remainder = arrangement.slice(towel.length)
+            if (is_possible(remainder,towels)) {
+                cache.set(arrangement, true)
+                // console.log("is possible")
+                return true
+            }
+        }
+    }
+    cache.set(arrangement, false)
+    // console.log("isnt possible")
+    return false
+}
+
+const p1 = function(filename) {
+    let [towels, arrangements] = parse(filename)
+    let count = 0
+    for (let arrangement of arrangements) {
+        if (is_possible(arrangement, towels)) {
+            count++
+        }
+    }
+    return count
+}
+
+const p2 = function(filename) {
+    let data = parse(filename)
+    return
+}
+
+const start1 = process.hrtime()
+const result1 = p1("data/d19.txt")
+const time1 = process.hrtime(start1)
+
+console.log("Day 19, part 1 answer:", result1, "took", time1[0], "seconds and", time1[1]/1000000, "milliseconds")
+
+
+const start2 = process.hrtime()
+const result2 = p2("data/d19.txt")
+const time2 = process.hrtime(start2)
+
+console.log("Day 19, part 2 answer:", result2, "took", time2[0], "seconds and", time2[1]/1000000, "milliseconds")
