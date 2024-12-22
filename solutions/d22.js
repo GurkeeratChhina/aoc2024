b43084d5ed2e4173768977977f76f9b705ecceed
GurkeeratChhina <gurkeerat.chhina@gmail.com>
2024-12-21 22:36:58 -0800

    part 2 done

--- solutions/d22.js
+++ solutions/d22.js
@@ -19,13 +19,31 @@ const my_binary = function(x, size) {
     return out
 }
 
-const pseudo = function (x) {
-    let temp = ((x << 6)^x)% (2**24)
-    // console.log(temp)
-    temp = (temp >> 5)^temp
-    // console.log(temp)
-    // console.log((temp << 11))
-    return ((temp << 11)^temp) % (2**24)
+const to_int = function(x) {
+    let sum = 0
+    for (let i = x.length-1; i >=0;i--){
+        sum *= 2
+        sum += x[i]
+    }
+    return sum
+}
+
+const update = function (x) {
+    let y = x.slice()
+    for (let i = 6; i < x.length; i++) {
+        y[i] = (x[i]+x[i-6])%2
+    }
+    x = y
+    y = x.slice()
+    for (let i = 0; i < x.length-5; i++) {
+        y[i] = (x[i]+x[i+5])%2
+    }
+    x = y
+    y = x.slice()
+    for (let i = 11; i < x.length; i++) {
+        y[i] = (x[i]+x[i-11])%2
+    }
+    return y
 }
 
 const p1 = function(filename) {
@@ -33,22 +51,70 @@ const p1 = function(filename) {
     let sum = 0
     for (let num of data) {
         let x = my_binary(num,24)
-        for (let i = 0; i < 10; i++) {
-            x = pseudo(x)
-            // console.log(i, temp)
+        // console.log(to_int(x))
+        for (let i = 0; i < 2000; i++) {
+            x = update(x)
         }
-        sum += temp
+        sum += to_int(x)
     }
     return sum
 }
 
 const p2 = function(filename) {
     let data = parse(filename)
-    return
+    first_occurances = new Map()
+    for (let num of data) {
+        let x = my_binary(num,24)
+        let prev = to_int(x)%10
+        let deltas = []
+        for (let i = 0; i < 4; i++) {
+            x = update(x)
+            let score = to_int(x)%10
+            deltas.push(score-prev)
+            prev = score
+        }
+        // console.log(deltas)
+        let key = JSON.stringify(deltas)
+        if (!first_occurances.has(key)) {
+            let val = new Map()
+            val.set(num, prev)
+            first_occurances.set(key, val)
+        } else if (!first_occurances.get(key).has(num)){
+            let val = first_occurances.get(key)
+            val.set(num, prev)
+        }
+        for (let i = 4; i < 2000; i++) {
+            x = update(x)
+            let score = to_int(x)%10
+            deltas.shift()
+            deltas.push(score-prev)
+            prev = score
+            let key = JSON.stringify(deltas)
+            if (!first_occurances.has(key)) {
+                let val = new Map()
+                val.set(num, prev)
+                first_occurances.set(key, val)
+            } else if (!first_occurances.get(key).has(num)){
+                let val = first_occurances.get(key)
+                val.set(num, prev)
+            }
+        }
+    }
+    // console.log(first_occurances)
+
+    let max = 0
+    for (let [key, map] of first_occurances) {
+        let sum = 0
+        for (let [num, score] of map) {
+            sum += score
+        }
+        max = Math.max(max, sum)
+    }
+    return max
 }
 
 const start1 = process.hrtime()
-const result1 = p1("data/d22a.txt")
+const result1 = p1("data/d22.txt")
 const time1 = process.hrtime(start1)
 
 console.log("Day 22, part 1 answer:", result1, "took", time1[0], "seconds and", time1[1]/1000000, "milliseconds")
